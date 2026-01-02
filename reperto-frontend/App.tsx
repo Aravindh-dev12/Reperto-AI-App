998e22d4c5ae2ec9e9d6de814f89521d2bda10db
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-01-02 09:18:46 +0530

    Update App.tsx

--- reperto-frontend/App.tsx
+++ reperto-frontend/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import RootStack from './src/navigation/RootStack';
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import RootStack from "./src/navigation/RootStack";
+
 export default function App() {
   return (
     <NavigationContainer>
       <RootStack />
-      <StatusBar style="dark" />
     </NavigationContainer>
   );
 }
